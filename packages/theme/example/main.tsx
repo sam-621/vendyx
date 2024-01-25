3bbb0d0781e34a4d136a1b054e72ec5ca4794048
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-24 22:37:15 -0700

    feat(theme):  add ThemeProvider

--- packages/theme/example/main.tsx
+++ packages/theme/example/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import { ThemeProvider } from '../src'
+
 import '../src/styles/tailwind.css'
 import '../src/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
