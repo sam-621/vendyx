5e8d5476ff8c34174ac059f1d7ed9f17998ac367
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-25 15:04:56 -0700

    feat(core):  add errors

---
+++ packages/core/src/lib/errors/errors.ts
@@ -0,0 +1,54 @@
+import { ErrorCode, ErrorMetadata, ErrorResult } from './errors.type';
+
+/**
+ * @description
+ * This is the error base class.
+ * All controlled errors should extend this class.
+ */
+export abstract class BusinessError implements ErrorResult {
+  constructor(
+    readonly code: ErrorCode,
+    readonly message: string,
+    readonly metadata?: ErrorMetadata,
+  ) {}
+}
+
+/**
+ * @description
+ * This error is thrown when an unexpected error occurs.
+ */
+export class InternalServerError extends BusinessError {
+  constructor(message: string, metadata?: ErrorMetadata) {
+    super(ErrorCode.INTERNAL_SERVER_ERROR, message, metadata);
+  }
+}
+
+/**
+ * @description
+ * This error is thrown when a user is not authorized to perform an action.
+ */
+export class UnauthorizedError extends BusinessError {
+  constructor(message: string, metadata?: ErrorMetadata) {
+    super(ErrorCode.UNAUTHORIZED, message, metadata);
+  }
+}
+
+/**
+ * @description
+ * This error is thrown when a user input is invalid.
+ */
+export class UserInputError extends BusinessError {
+  constructor(message: string, metadata?: ErrorMetadata) {
+    super(ErrorCode.USER_INPUT, message, metadata);
+  }
+}
+
+/**
+ * @description
+ * This error is thrown when a validation error occurs.
+ */
+export class ValidationError extends BusinessError {
+  constructor(message: string, metadata?: ErrorMetadata) {
+    super(ErrorCode.VALIDATION, message, metadata);
+  }
+}
---
+++ packages/core/src/lib/errors/errors.type.ts
@@ -0,0 +1,38 @@
+export type ErrorResult = {
+  code: ErrorCode;
+  message: string;
+};
+
+export enum ErrorCode {
+  /**
+   * Unexpected error.
+   *
+   * @description
+   * This error should be throw when the origin of the error is not clear.
+   * The only way to know the origin of the error is to check the logs.
+   */
+  INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR',
+  /**
+   * User is not authorized to perform an action.
+   *
+   * @description
+   * This error should be thrown when a user is not authorized to perform an action or access a resource.
+   */
+  UNAUTHORIZED = 'UNAUTHORIZED',
+  /**
+   * User input is invalid.
+   *
+   * @description
+   * This error should be thrown when a user input that comes from the client is invalid.
+   */
+  USER_INPUT = 'USER_INPUT',
+  /**
+   * Validation error.
+   *
+   * @description
+   * This error should be thrown when logic validation error occurs.
+   */
+  VALIDATION = 'VALIDATION',
+}
+
+export type ErrorMetadata = Record<string, unknown>;
