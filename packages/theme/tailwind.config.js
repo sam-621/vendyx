1f342969b789db3b97998166e2b458472466db7e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-24 19:16:07 -0700

    chore(theme):  add lib mode and example folder

--- packages/theme/tailwind.config.js
+++ packages/theme/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/example/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -1,7 +1,28 @@
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite config for lib mode, generates declaration files, builds for es and umd formats, and exports content from index.ts
+export default defineConfig(() => ({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // Generates declaration files (*.d.ts) from .ts(x) source files
+    dts({
+      include: ['src/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'lib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+}))
