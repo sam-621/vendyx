ae5680edb1964cf49bf060319154d07a75388116
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-25 18:29:12 -0700

    feat(core):  add option services

---
+++ packages/core/src/app/service/services/option.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { OptionEntity } from '@/app/persistance';
+
+@Injectable()
+export class OptionService {
+  constructor(
+    @InjectRepository(OptionEntity)
+    private optionRepository: Repository<OptionEntity>,
+  ) {}
+
+  async create(name: string) {
+    const optionToSave = this.optionRepository.create({ name });
+
+    await this.optionRepository.insert(optionToSave);
+
+    return optionToSave;
+  }
+}
