f0f6b38297b4aba772f9177f3fdd08c9083dc54f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-26 15:38:41 -0700

    feat(core):  add CloudinaryStorageProvider

---
+++ packages/core/src/lib/storage/cloudinary.provider.ts
@@ -0,0 +1,40 @@
+// cloudinary.service.ts
+import { Logger } from '@nestjs/common';
+import { v2 } from 'cloudinary';
+
+import { StorageProvider } from './storage.type';
+
+export class CloudinaryStorageProvider implements StorageProvider {
+  name: string;
+  code: string;
+
+  constructor(config: CloudinaryConfig) {
+    v2.config({
+      cloud_name: config.cloudName,
+      api_key: config.apiKey,
+      api_secret: config.cloudName,
+    });
+  }
+
+  async upload(file: string): Promise<string | null> {
+    try {
+      const fileUploaded = await v2.uploader.upload(file, {
+        folder: 'vendyx',
+      });
+
+      return fileUploaded.public_id;
+    } catch (error) {
+      Logger.error({
+        provider: this.code,
+        error,
+      });
+      return null;
+    }
+  }
+}
+
+type CloudinaryConfig = {
+  cloudName: string;
+  apiKey: string;
+  apiSecret: string;
+};
---
+++ packages/core/src/lib/storage/storage.type.ts
@@ -0,0 +1,29 @@
+export interface StorageProvider {
+  /**
+   * The provider name. This is used to display the provider name to the user.
+   *
+   * @description
+   * The name is not arbitrary, it's up to you to choose a name that makes sense for your provider.
+   *
+   * @example
+   * ```ts
+   * name: 'Google Cloud Storage'
+   * ```
+   */
+  name: string;
+
+  /**
+   * The provider code. This is used to identify the provider and should be unique.
+   *
+   * @description
+   * The name is not arbitrary, it's up to you to choose a name that makes sense for your provider.
+   *
+   * @example
+   * ```ts
+   * code: 'google-cloud-storage'
+   * ```
+   */
+  code: string;
+
+  upload(file: string): Promise<string | null>;
+}
