7d366b421dd5c0715d3707613d1ebb691be54641
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-27 13:50:48 -0700

    feat(core):  add GraphqlApiModule as dynamic module

--- packages/core/src/app/api/api.module.ts
+++ packages/core/src/app/api/api.module.ts
@@ -1,11 +1,9 @@
 import * as path from 'path';
 
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 
 import { AdminApiModule } from './admin';
+import { GraphqlApiModule } from './common/graphql-api.module';
 import { UploadModule } from './upload';
 
 const COMMON_SCHEMA_PATH = './common/**/*.schema.gql';
@@ -14,33 +12,12 @@ const ADMIN_API_SCHEMA_PATH = './admin/**/*.schema.gql';
 @Module({
   imports: [
     UploadModule,
-    AdminApiModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      // false to use apollo studio
-      playground: false,
-      // false in production, true in dev
-      includeStacktraceInErrorResponses: true,
-      driver: ApolloDriver,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      formatError: (error) => {
-        return {
-          message: error.message,
-          code: error.extensions?.code,
-        };
-      },
+    GraphqlApiModule.register({
+      include: [AdminApiModule],
       path: '/admin-api',
       typePaths: [COMMON_SCHEMA_PATH, ADMIN_API_SCHEMA_PATH].map((p) =>
         path.join(process.cwd(), './src/app/api', p),
       ),
-      definitions: {
-        path: path.join(
-          process.cwd(),
-          './src/app/api',
-          './common/types/gql.types.ts',
-        ),
-        outputAs: 'class',
-      },
-      include: [AdminApiModule],
     }),
   ],
 })
---
+++ packages/core/src/app/api/common/graphql-api.module.ts
@@ -0,0 +1,45 @@
+import * as path from 'path';
+
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DynamicModule, Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({})
+export class GraphqlApiModule {
+  static register(options: GraphqlApiModuleOptions): DynamicModule {
+    return {
+      imports: options.include,
+      ...GraphQLModule.forRoot<ApolloDriverConfig>({
+        // false to use apollo studio
+        playground: false,
+        // TODO: false in production, true in dev
+        includeStacktraceInErrorResponses: true,
+        driver: ApolloDriver,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        formatError: (error) => {
+          return {
+            message: error.message,
+            code: error.extensions?.code,
+          };
+        },
+        definitions: {
+          path: path.join(
+            process.cwd(),
+            './src/app/api/common/types/gql.types.ts',
+          ),
+          outputAs: 'class',
+        },
+        path: options.path,
+        typePaths: options.typePaths,
+        include: options.include,
+      }),
+    };
+  }
+}
+
+type GraphqlApiModuleOptions = {
+  include: any[];
+  typePaths: string[];
+  path: string;
+};
