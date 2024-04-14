8c53b056fd207695d929e842f5e4293506a2c42c
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-04-13 18:29:46 -0700

    feat(admin-ui):  add lang in login

--- packages/admin-ui/src/app/ui/login/components/login-form/login-form.tsx
+++ packages/admin-ui/src/app/ui/login/components/login-form/login-form.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@vendyx/theme';
 
 import { FormInput } from '@/components/forms';
+import { t } from '@/lib/locales';
 
 import { useLoginForm } from './use-login-form';
 
@@ -11,17 +12,17 @@ export const LoginForm = () => {
     <form onSubmit={onSubmit} className="flex flex-col gap-6">
       <FormInput
         {...register('username')}
-        label="Username"
+        label={t('login.form.input.username')}
         placeholder="admin"
         error={errors.username?.message}
       />
       <FormInput
         {...register('password')}
-        label="Password"
+        label={t('login.form.input.username')}
         placeholder="******"
         error={errors.password?.message}
       />
-      <Button isLoading={isSubmitting}>Login</Button>
+      <Button isLoading={isSubmitting}>{t('login.form.button.login')}</Button>
     </form>
   );
 };
--- packages/admin-ui/src/app/ui/login/page/login-page.tsx
+++ packages/admin-ui/src/app/ui/login/page/login-page.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '@/components/items';
+import { t } from '@/lib/locales';
 
 import { LoginForm } from '../components/login-form';
 
@@ -11,20 +12,16 @@ export const LoginPage = () => {
         </header>
         <footer>
           <blockquote className="flex flex-col gap-2">
-            <p className="text-lg text-white">
-              A functional and scalable minimal e-commerce admin that can be adjusted to any
-              user&apos;s requirement. Providing a full featured solution but focused on simplicity
-              at the same time
-            </p>
+            <p className="text-lg text-white">{t('login.testimony.text')}</p>
           </blockquote>
         </footer>
       </section>
       <section className="flex flex-col justify-center mx-auto w-[335px] gap-6">
         <div className="mx-auto">
-          <h1 className="text-center text-2xl font-semibold tracking-tight">Login into Vendyx</h1>
-          <p className="text-center text-sm text-muted-foreground">
-            Enter your username and password
-          </p>
+          <h1 className="text-center text-2xl font-semibold tracking-tight">
+            {t('login.form.title')}
+          </h1>
+          <p className="text-center text-sm text-muted-foreground">{t('login.form.subtitle')}</p>
         </div>
         <LoginForm />
       </section>
