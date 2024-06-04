434eb2c7358d8494ea36a2426c6332b7522d0166
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-06-03 17:31:48 -0700

    feat(admin-ui):  add useCreateProduct
    '

---
+++ packages/admin-ui/src/app/inventory/hooks/use-create-option.ts
@@ -0,0 +1,18 @@
+import { useGqlMutation } from '@/lib/gql';
+import { type CreateOptionInput } from '@/lib/vendyx/codegen/graphql';
+import { CreateOptionMutation } from '@/lib/vendyx/mutations';
+
+export const useCreateProduct = () => {
+  const { mutateAsync, isPending } = useGqlMutation(CreateOptionMutation);
+
+  const createOption = async (input: CreateOptionInput) => {
+    const { createOption: options } = await mutateAsync({ createOptionInput: input });
+
+    return options;
+  };
+
+  return {
+    createOption,
+    isLoading: isPending
+  };
+};
