01cc5839c0759cdc1bde29af55c9ec86651d83f0
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-06-22 19:28:24 -0700

    feat(admin-ui):  update ExtraUiModulePage

--- packages/admin-ui/src/app/extra-ui-module-page.tsx
+++ packages/admin-ui/src/app/extra-ui-module-page.tsx
@@ -1,7 +1,7 @@
 import { type FC } from 'react';
 
 export const ExtraUiModulePage: FC<Props> = ({ id }) => {
-  return <iframe src={id}></iframe>;
+  return <iframe className="w-full h-full" src={id}></iframe>;
 };
 type Props = {
   id: string;
