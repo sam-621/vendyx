eb5902427f643de303bba19c9a049c844907ba7a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-06-29 17:52:40 -0700

    feat(admin-ui):  add MassiveRemoveVariantButton

---
+++ packages/admin-ui/src/app/inventory/components/product-details/variant-details/variant-listing/variant-list-header/massive-remove-variant-button.tsx
@@ -0,0 +1,68 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button
+} from '@ebloc/theme';
+import { Trash2Icon } from 'lucide-react';
+
+import { useProductDetailsContext, useVariantsContext } from '@/app/inventory/context';
+import { ProductKeys, useMassiveVariantRemove } from '@/app/inventory/hooks';
+import { notification } from '@/lib/notifications';
+import { queryClient } from '@/lib/query-client';
+
+export const MassiveRemoveVariantButton = () => {
+  const { product } = useProductDetailsContext();
+  const { checkedVariants } = useVariantsContext();
+  const { massiveVariantRemove } = useMassiveVariantRemove();
+
+  const onRemove = async () => {
+    if (!product) return;
+
+    const notificationId = notification.loading('Removing variants...');
+
+    await massiveVariantRemove(
+      checkedVariants.map(id => id),
+      product
+    );
+
+    await queryClient.invalidateQueries({ queryKey: ProductKeys.single(product.slug) });
+
+    notification.dismiss(notificationId);
+    notification.success('Variants removed successfully');
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button type="button" variant="link" className="gap-1 text-destructive">
+          <Trash2Icon size={16} />
+          Delete selected
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="sm:max-w-[425px]">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="font-medium">
+            Remove {checkedVariants.length} variant{checkedVariants.length > 1 ? 's' : ''}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            It will remove {checkedVariants.length} variant{checkedVariants.length > 1 ? 's' : ''}{' '}
+            from your store. This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel type="button">Cancel</AlertDialogCancel>
+          <AlertDialogAction type="button" onClick={onRemove}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- packages/admin-ui/src/app/inventory/components/product-details/variant-details/variant-listing/variant-list-header/variant-list-header.tsx
+++ packages/admin-ui/src/app/inventory/components/product-details/variant-details/variant-listing/variant-list-header/variant-list-header.tsx
@@ -1,55 +1,22 @@
-import { type FC, useState } from 'react';
+import { type FC } from 'react';
 
-import { Button, Label } from '@ebloc/theme';
-import { Trash2Icon } from 'lucide-react';
+import { Label } from '@ebloc/theme';
 
-import { useProductDetailsContext, useVariantsContext } from '@/app/inventory/context';
-import { ProductKeys, useMassiveVariantRemove } from '@/app/inventory/hooks';
-import { notification } from '@/lib/notifications';
-import { queryClient } from '@/lib/query-client';
+import { useVariantsContext } from '@/app/inventory/context';
+
+import { MassiveRemoveVariantButton } from './massive-remove-variant-button';
 
 export const VariantListHeader: FC<Props> = ({ canShow }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const { product } = useProductDetailsContext();
   const { checkedVariants } = useVariantsContext();
-  const { massiveVariantRemove } = useMassiveVariantRemove();
 
   if (!canShow) {
     return null;
   }
 
-  const onRemove = async () => {
-    if (!product) return;
-
-    setIsLoading(true);
-
-    await massiveVariantRemove(
-      checkedVariants.map(id => id),
-      product
-    );
-
-    await queryClient.invalidateQueries({ queryKey: ProductKeys.single(product.slug) });
-
-    setIsLoading(false);
-    notification.success('Variants removed successfully');
-  };
-
   return (
     <div className="flex justify-between items-center border-y h-12 px-6">
       <Label>{checkedVariants.length ? `${checkedVariants.length} selected` : 'Variants'}</Label>
-      <div>
-        {checkedVariants.length > 0 && (
-          <Button
-            isLoading={isLoading}
-            variant="link"
-            className="gap-1 text-destructive"
-            onClick={onRemove}
-          >
-            {!isLoading && <Trash2Icon size={16} />}
-            Delete selected
-          </Button>
-        )}
-      </div>
+      <div>{checkedVariants.length > 0 && <MassiveRemoveVariantButton />}</div>
     </div>
   );
 };
