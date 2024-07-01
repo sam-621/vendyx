24bc1aa346794ee78618251f364eef1b7a227495
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-07-01 01:01:06 -0700

    feat(admin-ui):  collection search products works coorectly

--- packages/admin-ui/src/app/collections/components/collection-details/collection-products-table/collection-products-table.tsx
+++ packages/admin-ui/src/app/collections/components/collection-details/collection-products-table/collection-products-table.tsx
@@ -15,13 +15,18 @@ import {
   TableHeader,
   TableRow
 } from '@ebloc/theme';
-import { Trash2Icon } from 'lucide-react';
+import { XIcon } from 'lucide-react';
 
+import { CollectionKeys } from '@/app/collections/hooks';
+import { useSetProductsInCollection } from '@/app/collections/hooks/use-set-products-in-collection';
 import { type CommonCollectionFragment } from '@/lib/ebloc/codegen/graphql';
+import { notification } from '@/lib/notifications';
+import { queryClient } from '@/lib/query-client';
 
 import { CollectionProductsSearcherDialog } from './collection-products-searcher/collection-products-searcher-dialog';
 
 export const CollectionProductsTable: FC<Props> = ({ collection }) => {
+  const { setProductsInCollection } = useSetProductsInCollection();
   const products = collection.products.items;
 
   return (
@@ -29,53 +34,77 @@ export const CollectionProductsTable: FC<Props> = ({ collection }) => {
       <CardHeader className="flex flex-row justify-between items-center">
         <div>
           <CardTitle>Products</CardTitle>
-          <CardDescription>This collection contains 214 products</CardDescription>
+          <CardDescription>
+            This collection contains {products.length}{' '}
+            {products.length === 1 ? 'Product' : 'products'}
+          </CardDescription>
         </div>
         <CollectionProductsSearcherDialog collection={collection} />
       </CardHeader>
 
-      <CardContent className="flex flex-col gap-4">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Product</TableHead>
-              <TableHead>Stock</TableHead>
-              <TableHead>Enabled</TableHead>
-              <TableHead></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {products.map(product => (
-              <TableRow key={product.id}>
-                <TableCell>
-                  <Link
-                    to={`/products/${product.slug ?? ''}`}
-                    className="flex items-center gap-2 w-full"
-                  >
-                    {Boolean(product.assets.items.length) && (
-                      <img
-                        src={product.assets.items[0].source}
-                        alt={product.name}
-                        className="h-12 w-12 object-cover rounded-md"
-                      />
-                    )}
-                    <span>{product.name}</span>
-                  </Link>
-                </TableCell>
-                <TableCell>{product.variants.items.reduce((acc, v) => acc + v.stock, 0)}</TableCell>
-                <TableCell>
-                  <Badge variant={product.published ? 'default' : 'secondary'}>
-                    {product.published ? 'Published' : 'Disabled'}
-                  </Badge>
-                </TableCell>
-                <TableCell>
-                  <Trash2Icon size={16} className="cursor-pointer" />
-                </TableCell>
+      {Boolean(products.length) && (
+        <CardContent className="flex flex-col gap-4">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Product</TableHead>
+                <TableHead>Stock</TableHead>
+                <TableHead>Enabled</TableHead>
+                <TableHead></TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
+            </TableHeader>
+            <TableBody>
+              {products.map(product => (
+                <TableRow key={product.id}>
+                  <TableCell>
+                    <Link
+                      to={`/products/${product.slug ?? ''}`}
+                      className="flex items-center gap-2 w-full"
+                    >
+                      {Boolean(product.assets.items.length) && (
+                        <img
+                          src={product.assets.items[0].source}
+                          alt={product.name}
+                          className="h-12 w-12 object-cover rounded-md"
+                        />
+                      )}
+                      <span>{product.name}</span>
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    {product.variants.items.reduce((acc, v) => acc + v.stock, 0)}
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant={product.published ? 'default' : 'secondary'}>
+                      {product.published ? 'Published' : 'Disabled'}
+                    </Badge>
+                  </TableCell>
+                  <TableCell>
+                    <XIcon
+                      onClick={async () => {
+                        const id = notification.loading(`Removing ${product.name} from collection`);
+
+                        await setProductsInCollection(
+                          collection.id,
+                          products.filter(p => p.id !== product.id).map(p => p.id)
+                        );
+                        await queryClient.invalidateQueries({
+                          queryKey: CollectionKeys.single(collection.id)
+                        });
+
+                        notification.dismiss(id);
+                        notification.success(`"${product.name}" removed from collection`);
+                      }}
+                      size={16}
+                      className="cursor-pointer"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      )}
     </Card>
   );
 };
