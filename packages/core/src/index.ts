24f6954eac0eecbc094449af065670c21ad44157
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-07-19 14:00:35 -0700

    feat(core):  update index file that re exports all from core lib

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,17 +2,41 @@
   This file is used to export all the modules from the core package that will be used in the main application
 */
 
-export * from './main';
+/**
+ * Bootstrap method
+ */
+export { bootstrap } from './main';
+
+/**
+ * Business logic
+ */
 export * from './app/service/services';
 export * from './app/persistance/entities';
+export * from './app/security/security.service';
 
+/**
+ * Api logic
+ */
+export * from './app/api/common/guards';
+
+/**
+ * Config specifications
+ */
 export * from './app/config/payments/payment-integration.config';
 export * from './app/config/shipping/shipping-price-calculator.config';
 export * from './app/config/storage/storage-provider.config';
 export * from './app/config/admin-ui/admin-ui.config';
+export * from './app/config/ebloc.config';
+export * from './app/config/common';
 
+/**
+ * Plugin module
+ */
 export * from './app/plugin/ebloc.plugin';
 
+/**
+ * Integrations
+ */
 export * from './lib/storage';
 export * from './lib/shipping';
 export * from './lib/payments';
