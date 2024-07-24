c615955d28167115159d6e34781ec27e4bc9ed9f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-07-23 23:14:44 -0700

    fix(core):  build errors

--- packages/core/src/app/event-bus/event-bus.module.ts
+++ packages/core/src/app/event-bus/event-bus.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 
+import { EventBusService } from './event-bus.service';
 import { EventService } from './event.service';
 
+@Global()
 @Module({
   imports: [EventEmitterModule.forRoot()],
-  providers: [EventService]
+  providers: [EventService, EventBusService],
+  exports: [EventBusService]
 })
 export class EventBusModule {}
--- packages/core/src/app/event-bus/event.service.ts
+++ packages/core/src/app/event-bus/event.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 
-import { OrderEvent } from './events';
+import { CustomerEvent, OrderEvent } from './events';
 
 /**
  * 1.
@@ -16,5 +16,10 @@ export class EventService {
   @OnEvent(OrderEvent.PAID)
   handleOrderPaidEvent2(payload: any) {
     console.log('Order paid 2', payload);
+  }
+
+  @OnEvent(CustomerEvent.REGISTERED)
+  handleCustomerRegisteredEvent(payload: any) {
+    console.log('Customer registered', payload);
   }
 }
