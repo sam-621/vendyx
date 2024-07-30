fd971e45b809fffce1f2ad2824b6ed33c9ae4d45
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-07-29 20:54:42 -0700

    feat(admin-ui):  add FormInput

---
+++ packages/admin-ui/src/lib/shared/components/form/form-input.tsx
@@ -0,0 +1,46 @@
+import { type ComponentProps } from 'react';
+import { type FieldPath, type FieldValues } from 'react-hook-form';
+
+import { Input } from '@ebloc/theme';
+
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './form';
+
+export const FormInput = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  name,
+  label,
+  description,
+  placeholder,
+  ...rest
+}: Props<TFieldValues, TName>) => {
+  return (
+    <FormField
+      {...rest}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel>{label}</FormLabel>}
+          <FormControl>
+            <Input placeholder={placeholder} {...field} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+type Props<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = Pick<
+  ComponentProps<typeof FormField<TFieldValues, TName>>,
+  'control' | 'defaultValue' | 'disabled' | 'name'
+> & {
+  label?: string;
+  description?: string;
+  placeholder?: string;
+};
