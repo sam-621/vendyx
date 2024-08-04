70f553b04704514eb348cfc4adf3303134341625
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-08-03 23:38:41 -0700

    feat(admin-ui):  init nextjs app

--- packages/ui/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from 'tailwindcss';
-import { VendyxTailwindPreset } from './src/tailwind-preset';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [VendyxTailwindPreset],
-  content: ['./src/**/*.{ts,tsx}']
-} satisfies Config;
