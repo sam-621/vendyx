f35d85b5760a1679484918f224146f74db010a82
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-08-04 23:11:36 -0700

    feat(admin):  add codegen

--- apps/vendyx-admin/env.d.ts
+++ apps/vendyx-admin/env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
+    VENDYX_ADMIN_API_URL: string;
   }
 }
---
+++ apps/vendyx-admin/scripts/codegen.ts
@@ -0,0 +1,21 @@
+import { type CodegenConfig } from '@graphql-codegen/cli';
+
+const GQL_SCHEMA_URL = process.env.VENDYX_ADMIN_API_URL;
+
+const config: CodegenConfig = {
+  schema: GQL_SCHEMA_URL,
+  documents: ['./src/lib/shared/api/**/*.ts'],
+  generates: {
+    './src/lib/shared/api/codegen/': {
+      preset: 'client',
+      presetConfig: {
+        fragmentMasking: { unmaskFunctionName: 'getFragmentData' }
+      },
+      config: {
+        documentMode: 'string'
+      }
+    }
+  }
+};
+
+export default config;
