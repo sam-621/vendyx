a6703f3cb59af1be6bf1566c81ea68c7c27d412f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-08-05 01:03:09 -0700

    feat(admin):  theme switcher

--- apps/vendyx-admin/src/app/(auth)/layout.tsx
+++ apps/vendyx-admin/src/app/(auth)/layout.tsx
@@ -1,8 +1,13 @@
+import { ThemeSwitcher } from '@/lib/shared/components';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid items-center h-screen">
-      <div className="space-y-12">
-        <h1 className="text-center text-2xl font-medium">Vendyx.io</h1>
+      <div>
+        <div className="fixed top-6 right-6">
+          <ThemeSwitcher />
+        </div>
+        <h1 className="text-center text-2xl font-medium mb-12">Vendyx.io</h1>
         {children}
       </div>
     </div>
